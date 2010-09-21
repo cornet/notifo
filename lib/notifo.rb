36e6927e98a33fe1be3b59bb00f16dee53069850
John Schult <schulty@vitrue.com>
2010-09-21 01:14:57 -0400

    [CHANGE] More Ruby like method signatures, handles setup

--- lib/notifo.rb
+++ lib/notifo.rb
@@ -3,37 +3,46 @@ require 'cgi'
 require 'digest/sha1'
 
 class Notifo
+  cattr_accessor :service, :api_key
+  
   include HTTParty
   base_uri 'https://api.notifo.com/v1'
 
-  # Required Parameters
-  # username - notifo service username
-  # apikey - notifo service apisecret
-  def initialize(username, apisecret)
-    @auth = {:username => username, :password => apisecret}
+  # Convenience method for configuration
+  def Notifo.setup(&block)
+    yield self
   end
-  
+
   # Required Parameters
-  # username - notifo username to subscribe to your service
+  #   username - notifo service username
+  #   apikey - notifo service apisecret
+  def initialize(params={})
+    Notifo.service ||= params[:service]
+    Notifo.api_key ||= params[:api_key]
+    @auth = {:username => Notifo.service, :password => Notifo.api_key}
+  end
+
+  # Required Parameters
+  #   username - notifo username to subscribe to your service
   def subscribe_user(username)
-    options = { :body => {:username => username}, :basic_auth => @auth }
+    options = {:body => {:username => username}, :basic_auth => @auth}
     self.class.post("/subscribe_user", options)
   end
   
   # Required Parameters
-  # to - notifo username of recipient
-  # msg - message being sent; must be url encoded
+  #   to - notifo username of recipient
+  #   msg - message being sent; must be url encoded
   # Optional Parameters
-  # title - name of "notification event"
-  # uri - the uri that will be loaded when the notification is opened; if specified, must be urlencoded; if a web address, must start with http:// or https://
-  # label - label describing the "application" (used only if being sent from a User account; the Service label is automatically applied if being sent from a Service account) 
-  def post(username, msg, title=nil, uri=nil, label=nil)
-    options = { :body => {:to => username, :msg => msg, :label=>title, :title=>title, :uri=>uri}, :basic_auth => @auth }
+  #   title - name of "notification event"
+  #   uri - the uri that will be loaded when the notification is opened; if specified, must be urlencoded; if a web address, must start with http:// or https://
+  #   label - label describing the "application" (used only if being sent from a User account; the Service label is automatically applied if being sent from a Service account) 
+  def send_notification(params)
+    options = {:body => params, :basic_auth => @auth}
     self.class.post('/send_notification', options)
   end
 
   # Require Parameters
-  # params - the hash of params the webhook passed to you. All keys must be *Strings*, not symbols.
+  #   params - the hash of params the webhook passed to you. All keys must be *Strings*, not symbols.
   def verify_webhook_signature(params)
     signature = params['notifo_signature']
     other_notifo_params = params.reject {|key,val| !(key =~ /\Anotifo_/ && key != "notifo_signature")}
@@ -41,7 +50,6 @@ class Notifo
       params[key]
     end.join
     str << @auth[:password]
-
     signature == Digest::SHA1.hexdigest(CGI::escape(str))
   end
 end
